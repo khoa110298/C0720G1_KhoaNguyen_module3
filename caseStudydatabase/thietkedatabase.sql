4f95857e8b3d7a8d97e01d1f9168ab14ef06a807
khoa110298 <nguyenkhoa15011998@gmail.com>
2020-10-28 23:28:18 +0700

    up bai

--- caseStudydatabase/thietkedatabase.sql
+++ caseStudydatabase/thietkedatabase.sql
@@ -106,4 +106,117 @@ where ((year(now())-year(khachhang.nagysinh)) >= 18 and (year(now())-year(khachh
 and (khachhang.diachi = 'da nang' or khachhang.diachi = 'quang tri');
 
 -- câu 4
+ select khachhang.hoten, count(hopdong.idhopdong) as solanthue from khachhang 
+join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+ join loaikhach on khachhang.idLoaiKhach = loaikhach.idLoaiKhach
+ where loaikhach.tenLoaiKhach ='Diamond'
+ order by solanthue desc;
+
+-- câu 5
+
+select khachhang.idkhachhang, khachhang.hoten, loaikhach.tenloaikhach, hopdong.idhopdong,
+dichvu.tendichvu, ngaylamhopdong, ngayketthuc ,chiphithue,soluong,gia, 
+(chiphithue + soluong*gia) as tongtien
+from khachhang 
+left join loaikhach on khachhang.idLoaiKhach = loaikhach.idLoaiKhach
+left join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+left join dichvu on hopdong.idDichVu = dichvu.iddichvu
+left join hopdongchitiet on hopdong.idhopdong = hopdongchitiet.idhopdong
+left join dichvudikem on hopdongchitiet.idDichVuDiKem = dichvudikem.iddichvudikem;
+ 
+ -- câu 6
+ 
+ select dichvu.iddichvu, dichvu.tendichvu,dichvu.dientich,dichvu.chiphithue,loaidichvu.tenloaidichvu
+ from khachhang join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+ join dichvu on hopdong.iddichvu = dichvu.iddichvu
+ join loaidichvu on dichvu.idLoaiDichVu = loaidichvu.idLoaiDichVu  where not (month(ngaylamhopdong) in (1,2,3) and
+ year(ngaylamhopdong) = 2019);
+
+-- câu 7
+
+ select dichvu.iddichvu, dichvu.tendichvu,dichvu.dientich,songuoitoida,dichvu.chiphithue,loaidichvu.tenloaidichvu
+ from khachhang join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+ join dichvu on hopdong.iddichvu = dichvu.iddichvu
+ join loaidichvu on dichvu.idLoaiDichVu = loaidichvu.idLoaiDichVu  where (not year(ngaylamhopdong) = 2019) and
+ (year(ngaylamhopdong) = 2018);
+  
+-- câu 8
+
+select khachhang.hoten,count(khachhang.hoten) as solanlap from khachhang
+group by khachhang.hoten;
+select distinct khachhang.hoten from khachhang;
+select khachhang.hoten from khachhang
+union 
+select khachhang.hoten from khachhang;
+
+-- câu 9
+
+select month(hopdong.ngaylamhopdong) as thang,count(month(ngaylamhopdong)) as songuoidatphong
+from hopdong where
+year(ngaylamhopdong) = 2019
+group by month(ngaylamhopdong)
+order by month(ngaylamhopdong); 
+
+-- câu 10
+
+select hopdong.idhopdong,ngaylamhopdong,ngayketthuc,tiendatcoc,hopdongchitiet.soluong,
+count(idhopdongchitiet) as soluongdichvudikem
+from hopdong join hopdongchitiet on
+hopdong.idhopdong = hopdongchitiet.idhopdong
+join dichvudikem on hopdongchitiet.iddichvudikem = dichvudikem.iddichvudikem
+group by idhopdong
+order by idhopdong;
+
+-- câu 11
+
+select dichvudikem.iddichvudikem,dichvudikem.tendichvudikem,
+dichvudikem.gia,dichvudikem.donvi,khachhang.hoten
+from khachhang join loaikhach on
+khachhang.idLoaiKhach = loaikhach.idLoaiKhach
+join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+join hopdongchitiet on hopdong.idhopdong = hopdongchitiet.idhopdong
+join dichvudikem on hopdongchitiet.iddichvudikem = dichvudikem.iddichvudikem
+where tenLoaiKhach = 'Diamond' and (diachi = 'quang ngai' or diachi = 'vinh')
+order by iddichvudikem;
+
+-- câu 12
+
+select hopdong.idhopdong,nhanvien.hoten,khachhang.hoten,khachhang.sdt,
+dichvu.tendichvu,hopdongchitiet.soluong,tiendatcoc from hopdong
+join nhanvien on hopdong.idNhanVien = nhanvien.idNhanVien
+join khachhang on hopdong.idKhachHang = khachhang.idKhachHang
+join dichvu on hopdong.idDichVu = dichvu.iddichvu
+join hopdongchitiet on hopdong.idhopdong = hopdongchitiet.idhopdong
+where (year(ngaylamhopdong) = 2019 and month(ngaylamhopdong) in(10,11,12)) 
+and not (year(ngaylamhopdong) = 2019 and month(ngaylamhopdong) in(1,2,3,4,5,6));
+
+-- câu 13 
+
+select dichvudikem.iddichvudikem,dichvudikem.tendichvudikem,dichvudikem.gia,dichvudikem.donvi,
+dichvudikem.trangthaikhadung,count(tendichvudikem) as solansudung from khachhang 
+join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+join hopdongchitiet on hopdong.idhopdong = hopdongchitiet.idhopdong
+join dichvudikem on hopdongchitiet.idDichVuDiKem = dichvudikem.iddichvudikem where 
+(select  count(tendichvudikem) from khachhang 
+join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+join hopdongchitiet on hopdong.idhopdong = hopdongchitiet.idhopdong
+join dichvudikem on hopdongchitiet.idDichVuDiKem = dichvudikem.iddichvudikem
+group by tendichvudikem
+order by count(tendichvudikem) desc limit 1)
+group by tendichvudikem
+order by count(tendichvudikem) desc;
+
+-- câu 14
+ 
+ select hopdong.idhopdong, loaidichvu.tenloaidichvu,dichvudikem.tendichvudikem,
+ count(hopdongchitiet.iddichvudikem) as solansudung from khachhang
+ join hopdong on khachhang.idKhachHang = hopdong.idKhachHang
+join dichvu on hopdong.iddichvu = dichvu.iddichvu
+join loaidichvu on dichvu.idLoaiDichVu = loaidichvu.idLoaiDichVu
+join hopdongchitiet on hopdong.idhopdong = hopdongchitiet.idhopdong 
+join dichvudikem on hopdongchitiet.iddichvudikem = dichvudikem.iddichvudikem
+group by hopdongchitiet.iddichvudikem
+having count(hopdongchitiet.iddichvudikem) = 1
+order by idhopdong;
+ 
 
